2a693e78b9bda3af2a84ca8e4fbbf0a4e9aeddf2
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-04-21 22:27:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -40,11 +40,33 @@ function calculaTempo(tempoObjetivo) {
             return "Prazo Finalizado";
 }
 
+   /* aula 24/04
+     if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
+}
+*/
+
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
+
+/* aula 22/04
+function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+*/
 
 function comecaCronometro(){
     atualizaCronometro();
