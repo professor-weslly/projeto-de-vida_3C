1cc2085a5421d5bac03b2445fe31a6990d3c9be3
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-05-01 22:13:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
-   botoes[i].onclick = function () {
+    botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
@@ -22,6 +23,7 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -33,29 +35,27 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- if (tempoFinal > 0){
-     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-        } else {
-            return "Prazo Finalizado";
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos"; 
+    } else {
+        return "Prazo Finalizado"; 
+    }
 }
-
-   /* aula 24/04
-     if (tempoFinal > 0){
+/* aula 02/05 - substituir a função acima
+ if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
     }
 }
 */
-
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
-/* aula 22/04
+/* aula 02/05 - substituir a função acima
 function atualizaCronometro(){
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
