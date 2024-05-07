9ca7f8c2c72a37c5b7a507fd59347cf9d7097500
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-05-07 07:38:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -53,6 +53,16 @@ function atualizaCronometro(){
     }
 }
 
+/*Substituir função acima 07/05
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
+*/
+
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
